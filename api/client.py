183fc87023aa38c5e03b99002fcc38a6f3014862
Aishwarya-rathudi <x21222762@student.ncirl.ie>
2026-09-15 00:34:11 +0100

    Update client.py

--- api/client.py
+++ api/client.py
@@ -1,27 +1,28 @@
 import requests
 import streamlit as st
 
-def get_openai_response(input_text):
-    response=requests.post("http://localhost:8000/essay/invoke",
-    json={'input':{'topic':input_text}})
-
+def get_essay_response(input_text):
+    response = requests.post(
+        "http://localhost:8000/essay/invoke",
+        json={'input': {'topic': input_text}}
+    )
     return response.json()['output']['content']
 
-def get_ollama_response(input_text):
-    response=requests.post(
-    "http://localhost:8000/poem/invoke",
-    json={'input':{'topic':input_text}})
+def get_poem_response(input_text):
+    response = requests.post(
+        "http://localhost:8000/poem/invoke",
+        json={'input': {'topic': input_text}}
+    )
+    return response.json()['output']['content']
 
-    return response.json()['output']
+## streamlit framework
 
-    ## streamlit framework
-
-st.title('Langchain Demo With LLAMA2 API')
-input_text=st.text_input("Write an essay on")
-input_text1=st.text_input("Write a poem on")
+st.title('Langchain Demo With Groq API')
+input_text = st.text_input("Write an essay on")
+input_text1 = st.text_input("Write a poem on")
 
 if input_text:
-    st.write(get_openai_response(input_text))
+    st.write(get_essay_response(input_text))
 
 if input_text1:
-    st.write(get_ollama_response(input_text1))
+    st.write(get_poem_response(input_text1))
