a0763792a4a2ed8e2d6d86eb37a3d7374ad82e6e
Aishwarya-rathudi <x21222762@student.ncirl.ie>
2026-09-15 21:26:28 +0100

    Update app.py

--- groq/app.py
+++ groq/app.py
@@ -28,7 +28,7 @@ if "vector" not in st.session_state:
 st.title("ChatGroq Demo")
 llm=ChatGroq(
              groq_api_key=groq_api_key,
-             model_name="llama-3.3-70b-versatile",
+             model_name="openai/gpt-oss-20b",
              temperature=0,
              max_tokens=500
             )
